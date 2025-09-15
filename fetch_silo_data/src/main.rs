e241a15741cc26d537748953d64cc640f243aa68
Gordon J. Köhn <gordon.koehn@dbsse.ethz.ch>
2025-09-15 15:17:34 +0200

    WIP

--- fetch_silo_data/src/main.rs
+++ fetch_silo_data/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest;
+use tokio;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let url = "https://api.db.wasap.genspectrum.org/covid/sample/details?samplingDateFrom=2025-06-15&samplingDateTo=2025-09-15&limit=100&dataFormat=JSON&downloadAsFile=false";
+
+    println!("Fetching data from: {}", url); 
+
+    Ok(())
 }
